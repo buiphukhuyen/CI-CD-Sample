186fd0e23e9e5029400c174114f783752b7bb6f4
Bui Phu Khuyen <khuyenpb@gmail.com>
2026-03-22 13:18:14 +0700

    first commit

---
+++ src/main/java/com/example/phukhuyen/PhukhuyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.phukhuyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhukhuyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhukhuyenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phukhuyen/PhukhuyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phukhuyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhukhuyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
